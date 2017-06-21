ee55330eb84377d3e1f69c9f91c63b1e5441a021
Juan Carlos <jcromero@viewnext.com>
2017-06-21 17:46:50 +0200

    Cap 107

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {Lista, ListaItem} from '../../app/clases/index';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: 'agregar.component.html',
 })
 export class AgregarComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  nombreLista: string;
+  nombreItem = "";
+  items: ListaItem[] = [];
 
+  constructor() { }
 
+  ngOnInit() { }
+
+  agregarItem() {
+
+    if (this.nombreItem.length == 0) {
+      return;
+    }
+
+    let items = new ListaItem();
+    items.nombre=this.nombreItem;
+    this.items.push(items);
+    this.nombreItem = ""
+  }
+
+  eliminarItem(index:number) {
+
+    this.items.splice(index,1);
+
+  }
 }
