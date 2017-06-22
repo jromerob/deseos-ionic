e75525b03253c1b6b7b44a00f7d53a546e4b176e
Juan Carlos <jcromero@viewnext.com>
2017-06-22 17:03:54 +0200

    clase 111

--- src/app/services/lista-deseos.services.ts
+++ src/app/services/lista-deseos.services.ts
@@ -30,4 +30,9 @@ export class ListaDeseosService {
     this.listas.push(lista);
     this.actualizarData();
   }
+
+  eliminarLista(idx:number){
+    this.listas.splice(idx,1);
+    this.actualizarData();
+  }
 }
--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
+import {Lista, ListaItem} from '../../app/clases/index';
+import {ListaDeseosService} from '../../app/services/lista-deseos.services';
+
+
 
 @Component({
   selector: 'app-detalle',
@@ -7,16 +11,28 @@ import {NavController, NavParams} from 'ionic-angular';
 })
 export class DetalleComponent implements OnInit {
 
-idx:number;
-lista:any;
+  idx: number;
+  lista: any;
 
   constructor(
-    public navCtrl:NavController,
-    public navParams: NavParams
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public listaDeseosService: ListaDeseosService
   ) {
-    this.idx=navParams.get("idx");
-    this.lista=navParams.get("lista");
-  console.log(navParams) }
+    this.idx = navParams.get("idx");
+    this.lista = navParams.get("lista");
+    console.log(navParams)
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  actualizar(item: ListaItem) {
+    item.completado = !item.completado
+    this.listaDeseosService.actualizarData();
+  };
+
+  eliminarLista() {
+    this.listaDeseosService.eliminarLista(this.idx);
+    this.navCtrl.pop();
+  }
 }
